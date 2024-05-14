6711efcde23b8b2395482009b338c40f5133ba46
Riyadh Alghamdi <you@example.com>
2024-05-15 01:37:36 +0300

    Riyadh Alghamdi

---
+++ app/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def intro() -> None:
+    st.title('Unveiling Job Market Trends in Saudi Arabia: The Treasure Hunt')
+    st.
+
+def body() -> None:
+    pass
+
+def conclusion():
+    pass
+
+def app():
+    intro()
+    body()
+    conclusion()
+
+if __name__ == '__main__':
+    app()
